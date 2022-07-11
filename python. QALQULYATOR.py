8a5ff6a5c2e54830e2e7833d500c5641f0ed339f
Yodgorbek Boltaev <boltaevyodgorbek@gmail.com>
2022-07-11 15:59:00 +0300

    first commit

---
+++ python. QALQULYATOR.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 10 20:29:43 2022
+
+@author: Ogabek
+"""
+
+
+print("Pythontilida yozilgan kalkulyator")    
+def calculator(son1,son2):
+    """Python tilida yozilgan kalkulyator"""
+    son = {'son1':son1,
+           'son2':son2}
+           #    'son2':son2}
+           #    't_joy':t_joy,
+            #   'email':email,
+             #  'tel_raqami':tel_raqami}
+    return son
+
+print("\nArifmetik amallarni bajarish uchun son kiriting\n")
+san = []
+tarix = []
+
+while True:
+    son1 = int(input("Birinchi sonni kiriting: "))
+    son2 = int(input("Ikkinchi sonni kiriting kiriting: "))
+
+    print(san)
+    san.append(calculator(son1,son2))
+    print(san)
+    javob = input("Yana son qo'shasizmi?  (yes/no) \nTarixni ko'rish uchun (tarix) deb yozing")
+  #  javob += "(Tarixni tozalash uchun (new) deb yozing)"
+    if javob=='no':
+        break
+        
+        
+print(f"{son1 + son2}")
+      
+print(san)
+
+for sonlar in san:
+    hisob = (f"Qo'shish: {son1}+{son2} = {son1+son2} \n"
+          f"Ayirish: {son1}-{son2} = {son1-son2} \n\n"
+          f"Ko'paytirish: {son1}*{son2} = {son1*son2} \n\n"
+          f"Bo'lish: {son1}/{son2} = {son1/son2} \n\n"
+          f"O'nlik bo'lish: {son1}//{son2} = {son1//son2} \n\n"
+          f"Daraja: {son1}**{son2} = {son1**son2} \n\n"
+          f"Bo'linmani qoldig'i: {son1}%{son2} = {son1%son2} \n\n"
+          f"Katta son: {son1}>{son2} = {son1>son2} \n\n"
+          f"Kichik son: {son1}<{son2} = {son1<son2} \n\n")
+tarix.append(hisob)
+ 
+print(hisob)
+
+print(f"Sizning tarixingiz {tarix}")
